f53c0a191ae6105d0bc8ccb8fb6ca045aefc65bf
Yony Calsin <helloyonycalsin@gmail.com>
2021-12-03 13:22:50 -0500

    feat: 🎸 resolve the challenge day 03

--- challenge-03/main.test.ts
+++ challenge-03/main.test.ts
@@ -1,17 +1,27 @@
 import { isValid } from "./main";
 
-describe("challenge - 03", () => {
-  it("should return the correct value", () => {
+describe("challenge - 03 -> should return the correct value", () => {
+  it("bici coche (balón) bici coche peluche", () => {
     expect(isValid("bici coche (balón) bici coche peluche")).toBeTruthy();
+  });
 
+  it("(muñeca) consola bici", () => {
     expect(isValid("(muñeca) consola bici")).toBeTruthy();
+  });
 
+  it("bici coche (balón bici coche", () => {
     expect(isValid("bici coche (balón bici coche")).toBeFalsy();
+  });
 
+  it("peluche (bici [coche) bici coche balón", () => {
     expect(isValid("peluche (bici [coche) bici coche balón")).toBeFalsy();
+  });
 
+  it("(peluche {) bici", () => {
     expect(isValid("(peluche {) bici")).toBeFalsy();
+  });
 
+  it("() bici", () => {
     expect(isValid("() bici")).toBeFalsy();
   });
 });
--- challenge-03/main.ts
+++ challenge-03/main.ts
@@ -1,7 +1,40 @@
-export function isValid(letter: string) {
-  const wrongLetterGroups = letter.match(
-    /(\(\))|(\[\])|(\([a-z\s\[\]\{\}]+\))/gi
-  );
+import isNil from "../internal/is-nil";
 
-  return !wrongLetterGroups?.length;
+export function isValid(letter: string) {
+  const characters = letter.split("");
+
+  const tokens = {
+    start: null,
+    end: null,
+  };
+
+  for (let i = 0; i < characters.length; i++) {
+    switch (characters[i]) {
+      case "(":
+        tokens.start = i;
+        break;
+      case ")":
+        tokens.end = i;
+    }
+  }
+
+  if (!isNil(tokens.start) && isNil(tokens.end)) {
+    return false;
+  }
+
+  const group = letter.substring(tokens.start, tokens.end + 1);
+
+  const hasWrongGroupContent = /[\[\{]/gi.test(group);
+
+  if (hasWrongGroupContent) {
+    return false;
+  }
+
+  const hasEmptyGroupContent = group.replace(/(^\()|(\)$)/gi, "").length;
+
+  if (hasEmptyGroupContent === 0) {
+    return false;
+  }
+
+  return true;
 }
